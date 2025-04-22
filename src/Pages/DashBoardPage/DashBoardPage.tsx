ab2e9349e5df39e6c20b0d4600cc87b2ccb5cc9c
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-04-22 19:09:58 +0600

    The bar chart is also added. Now make it functional

--- src/Pages/DashBoardPage/DashBoardPage.tsx
+++ src/Pages/DashBoardPage/DashBoardPage.tsx
@@ -1,16 +1,20 @@
 import { SalesChart } from "@/Components/Charts/SalesChart";
-
+import { SalesbarChartByYear } from "@/Components/Charts/SalesBarChartByYear";
 export default function DashBoardPage() {
   return (
     <>
       <div className="px-8 py-4">
-        <div className="text-2xl font-semibold mb-2">
-          DashBoard
-        </div>
-        <hr className="border-1 border-indigo-300 shadow-lg shadow-black"/>
+        <div className="text-2xl font-semibold mb-2">DashBoard</div>
+        <hr className="border-1 border-indigo-300 shadow-lg shadow-black" />
         {/* Chart */}
-        <div className="w-full max-w-2xl h-96 mt-8">
-          <SalesChart />
+
+        <div className="flex fle-col gap-8">
+          <div className="w-full max-w-2xl h-96 mt-8">
+            <SalesChart />
+          </div>
+          <div className="w-full max-w-2xl mt-8 ">
+            <SalesbarChartByYear/>
+          </div>
         </div>
       </div>
     </>
