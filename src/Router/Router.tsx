4fb16cdede4e87189da54fbd120116f3a69835f7
lihazinoob <lihazi2007038@stud.kuet.ac.bd>
2025-04-22 21:40:19 +0600

    Maybe

--- src/Router/Router.tsx
+++ src/Router/Router.tsx
@@ -1,5 +1,5 @@
 import { createBrowserRouter } from "react-router-dom";
-import AuthLayout from "@/Layout/Auth/AuthLayout";
+
 import DashBoardLayout from "@/Layout/DashBoard/DashBoardLayout";
 import DashBoardPage from "@/Pages/DashBoardPage/DashBoardPage";
 import ProductManagementPage from "@/Pages/ProductManagementPage/ProductManagementPage";
